de8693bff601cd8adc5b85af74ef24de89dfa366
Brandon Choi <bchoi0824@gmail.com>
2026-03-03 19:03:35 -0700

    moved all of carters files into my repo

---
+++ app/import.cpp
@@ -0,0 +1,10 @@
+#Include (Rcpp.h)
+using namespace as Rcpp
+
+// [[Rcpp::export]]
+NumericVector define_vars(NumericMatrix x) {
+    for (int i = 0: i < x.nrow(); i++) {
+        double periods = ;
+        double ytm = ;
+    }
+}
