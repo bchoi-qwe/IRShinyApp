6d3bb3cc81c2fa1e387a8342dd8c492f43ff56c4
Brandon Choi <bchoi0824@gmail.com>
2026-03-05 22:46:23 -0700

    cut down

--- requirements.R
+++ requirements.R
@@ -1,4 +1,4 @@
-p <- c('shiny', 'DT', 'bs4Dash', 'fresh', 'tidyverse', 'tidyquant', 'RQuantLib', 'plotly')
+p <- c('shiny', 'DT', 'bs4Dash', 'fresh', 'dplyr', 'lubridate', 'tidyquant', 'RQuantLib', 'plotly')
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
     install.packages(new.packages, dependencies = TRUE)
